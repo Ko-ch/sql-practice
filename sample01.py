a56fc7334941835d96fa15e50ed2c4bbb11dca5b
Koichiro <koichiro_takagi@outlook.jp>
2019-09-25 10:05:12 +0900

    SQLの作成をしました

---
+++ sample01.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+"""
+目標：PythonでSQLを使ってテーブルをつくる
+"""
+
+
+def main():
+    conn = sqlite3.connect('users_db.sqlite')
+    cursor = conn.cursor()
+
+    # SQLの準備
+    sql = """CREATE TABLE users (
+                name TEXT,
+                age INTEGER
+                )"""
+
+    # SQL実行
+    cursor.execute(sql)
+
+    # コミット
+    conn.commit()
+
+    # 接続を切る
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
