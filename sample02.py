4e8eae4d892b8de0d6865998ad2101cb8efd0aa4
Koichiro <koichiro_takagi@outlook.jp>
2019-09-25 10:32:42 +0900

    初めてのINSERT文を発行した

--- sample02.py
+++ sample02.py
@@ -1,5 +1,26 @@
+"""
+目標：users.sqlite に INSERT文を発行する
+"""
+
+import sqlite3
+
+
 def main():
-    pass
+    conn = sqlite3.connect('users_db.sqlite')
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO users (name, age) VALUES('Bob', 15);"
+    conn.execute(sql)
+
+    sql = "INSERT INTO users (name, age) VALUES('Tom', 27);"
+    conn.execute(sql)
+
+    sql = "INSERT INTO users (name, age) VALUES('Ken', 77);"
+    conn.execute(sql)
+
+    conn.commit()
+
+    conn.close()
 
 
 if __name__ == "__main__":
